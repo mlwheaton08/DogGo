7d73578aac9529b038bac0a4b76df4f257071d2a
Mason Wheaton <mlwheaton06@gmail.com>
2023-04-06 20:46:18 -0500

    Midway through ch 3 exercises. Need to add get dog by id so can finish the Edit method

---
+++ DogGo/Controllers/DogsController.cs
@@ -0,0 +1,94 @@
+﻿using DogGo.Models;
+using DogGo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogGo.Controllers
+{
+    public class DogsController : Controller
+    {
+        private readonly IDogRepository _dogRepo;
+
+        public DogsController(IDogRepository dogRepo)
+        {
+            _dogRepo = dogRepo;
+        }
+
+        // GET: DogsController
+        public ActionResult Index()
+        {
+            List<Dog> dogs = _dogRepo.GetAllDogs();
+            return View(dogs);
+        }
+
+        // GET: DogsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DogsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DogsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Dog dog)
+        {
+            try
+            {
+                _dogRepo.AddDog(dog);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(dog);
+            }
+        }
+
+        // GET: DogsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DogsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DogsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DogsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DogGo/Repositories/IDogRepository.cs
@@ -0,0 +1,12 @@
+﻿using DogGo.Models;
+
+namespace DogGo.Repositories
+{
+    public interface IDogRepository
+    {
+        List<Dog> GetAllDogs();
+        void AddDog(Dog dog);
+        void UpdateDog(Dog dog);
+        void DeleteDog(int dogId);
+    }
+}
