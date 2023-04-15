f7b18027dfce112aa804c1e1fdb718bb4f16fb2b
Mason Wheaton <mlwheaton06@gmail.com>
2023-04-14 21:06:13 -0500

    chapter 7 finished

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -1,12 +1,29 @@
-﻿namespace DogGo.Models;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models;
 
 public class Owner
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Dawg, add a name.")]
+    [MaxLength(50)]
     public string Name { get; set; }
+
+    [EmailAddress]
+    [Required]
     public string Email { get; set; }
+
+    [Required]
+    [StringLength(55, MinimumLength = 6)]
     public string Address { get; set; }
+
+    [Phone]
+    [DisplayName("Phone Number")]
     public string Phone { get; set; }
+
+    [Required]
     public int NeighborhoodId { get; set; }
     public Neighborhood Neighborhood { get; set; }
     public List<Dog> Dogs { get; set; } = new List<Dog>();
--- DogGo/Models/Walks.cs
+++ DogGo/Models/Walks.cs
@@ -1,8 +1,12 @@
-﻿namespace DogGo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models;
 
 public class Walks
 {
     public int Id { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
     public DateTime Date { get; set; }
     public int Duration { get; set; }
     public int WalkerId { get; set; }
